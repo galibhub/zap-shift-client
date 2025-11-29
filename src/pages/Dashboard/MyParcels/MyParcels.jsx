620543278236d523b4a6c729a50a4febe193d0fd
Ibrahim Ahmed Galib <galib@Ibrahims-Mac-mini.local>
2025-11-29 12:13:20 +0600

    Implemented paid status using patch api

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -64,7 +64,7 @@ const MyParcels = () => {
 
    const res=await axiosSecure.post('/payment-checkout-session',paymentInfo);
    console.log(res.data.url)
-   window.location.href=res.data.url;
+   window.location.assign(res.data.url);
   }
 
   return (
--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const PaymentSuccess = () => {
+    const [searchParams]=useSearchParams();
+    const sessionId=searchParams.get('session_id')
+
+    const axiosSecure=useAxiosSecure();
+    console.log(sessionId)
+
+    useEffect(()=>{
+if(sessionId){
+  axiosSecure.patch(`/payment-success?session_id=${sessionId}`)
+  .then(res=>{
+    console.log(res.data)
+  })
+
+  .catch(error => {
+                    console.error('Payment update error:', error)
+                })
+}
+    },[sessionId,axiosSecure])
+
     return (
-        <div>
-            <h2 className='text-4xl'>Payment successfull</h2>
+        <div className="p-8">
+            <h2 className='text-4xl font-bold text-green-600'>Payment Successful! ✓</h2>
+            <p className="mt-4">Session ID: {sessionId}</p>
         </div>
     );
 };
