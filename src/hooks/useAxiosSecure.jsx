e179a0ffd3a17d97b84f646af85523b822e50611
Ibrahim Ahmed Galib <galib@Ibrahims-Mac-mini.local>
2025-11-30 11:27:15 +0600

    verify token

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,11 +1,53 @@
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React, { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
-const axiosSecure=axios.create({
-    baseURL:'http://localhost:3000'
-})
+const axiosSecure = axios.create({
+  baseURL: "http://localhost:3000",
+});
 const useAxiosSecure = () => {
-    return axiosSecure;
+  const { user, logOut } = useAuth();
+  const navigate=useNavigate()
+
+  //token verify
+  useEffect(() => {
+    //intercept request
+    const reqInterceptor = axiosSecure.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`;
+
+      return config;
+    });
+    //interceptor response
+    const resInterceptor = axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        // Handle error
+        console.error("Response error:", error);
+
+        const statusCode = error.status;
+
+        if (statusCode === 401 || statusCode === 403) {
+          logOut()
+          .then(()=>{
+            navigate('/login')
+          })
+        }
+
+        return Promise.reject(error);
+      }
+    );
+
+    //interceptor eject
+    return () => {
+      axiosSecure.interceptors.request.eject(reqInterceptor);
+      axiosSecure.interceptors.response.eject(resInterceptor);
+    };
+  }, [user]);
+
+  return axiosSecure;
 };
 
 export default useAxiosSecure;
--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,33 +1,43 @@
-import React, { useEffect } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const PaymentSuccess = () => {
-    const [searchParams]=useSearchParams();
-    const sessionId=searchParams.get('session_id')
+  const [searchParams] = useSearchParams();
+  const [paymentInfo,setPaymentInfo]=useState({})
+  const sessionId = searchParams.get("session_id");
 
-    const axiosSecure=useAxiosSecure();
-    console.log(sessionId)
+  const axiosSecure = useAxiosSecure();
+  console.log(sessionId);
 
-    useEffect(()=>{
-if(sessionId){
-  axiosSecure.patch(`/payment-success?session_id=${sessionId}`)
-  .then(res=>{
-    console.log(res.data)
-  })
 
-  .catch(error => {
-                    console.error('Payment update error:', error)
-                })
-}
-    },[sessionId,axiosSecure])
+  useEffect(() => {
+    if (sessionId) {
+      axiosSecure
+        .patch(`/payment-success?session_id=${sessionId}`)
+        .then((res) => {
+          console.log(res.data);
+          setPaymentInfo({
+            transactionId:res.data.transactionId,
+            trackingId:res.data.trackingId
+          })
+        })
 
-    return (
-        <div className="p-8">
-            <h2 className='text-4xl font-bold text-green-600'>Payment Successful! ✓</h2>
-            <p className="mt-4">Session ID: {sessionId}</p>
-        </div>
-    );
+        .catch((error) => {
+          console.error("Payment update error:", error);
+        });
+    }
+  }, [sessionId, axiosSecure]);
+
+  return (
+    <div className="p-8">
+      <h2 className="text-4xl font-bold text-green-600">
+        Payment Successful! ✓
+      </h2>
+      <p className="mt-4">Transaction ID: {paymentInfo.transactionId}</p>
+      <p className="mt-4"> ParcelTracking ID: {paymentInfo.trackingId}</p>
+    </div>
+  );
 };
 
 export default PaymentSuccess;
